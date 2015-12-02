7335283e0cb2b60c68cc90f0e70718accd6f92ad
Adriano Flechilla <a.flechilla91@gmail.com>
2015-12-02 11:21:41 -0500

    -Done the processor module. -Take the stopwords from a file or some idea clever. -Gonna do the indexer module next.

--- text_processor.py
+++ text_processor.py
@@ -1,80 +1,133 @@
 
 import re
+from os import listdir, stat
 
-STOP_WORDS=None
+from nltk.stem.lancaster import LancasterStemmer
+from json import JSONDecoder, JSONEncoder
 
-def load():
-    STOP_WORDS=loadStopwords()
-    tl=open('data/training.1600000.processed.noemoticon.csv','r')
-    # tl=open('testdata.manual.2009.06.14.csv','r')
-    line=tl.readline()
-    proc_tweets=[]
-    while line:
-        temp=line.split(',')
-        proc_tweets.append({int(re.sub('"','',temp[0])):processTweet(temp[-1])})
-        line=tl.readline()
-    tl.close()
-    return proc_tweets
+# TODO: enter dir with the docs
+class Text_Processor:
 
-def loadStopwords():
-    stopwords = set(["URL","USER"])
-    fp = open('data/stopwords_en.txt', 'r')
-    line = fp.readline()
-    while line:
-        line = line.strip('\n')
-        stopwords.add(line)
-        line = fp.readline()
-    fp.close()
-    return stopwords
+    def __init__(self):
+        self.terms=[]
+        self.load_stopwords()
 
-# start process_tweet
-# **taken from internet
-def processTweet(tweet):
-    # Convert to lower case
-    tweet = tweet.lower()
-    # Convert www.* or https?://* to URL
-    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
-    # Convert @username to USER
-    tweet = re.sub('@[^\s]+', 'USER', tweet)
-    # Remove additional white spaces
-    tweet = re.sub('[\s]+', ' ', tweet)
-    # Replace #word with word
-    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
-    # trim
-    tweet = tweet.strip('\'"')
 
-    #delete not letter chars
-    tweet=re.sub('[=|+|,|.|?|!|*|;|:|(|)|&|-|\|[|\]]','', tweet)
+    def process(self, json):
+        dec_json=JSONDecoder().decode(json)
+        if dec_json['action']=='process':
+            return self.lex_process(dec_json['data'])
 
-    #delete consecutive chars with the same value
-    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
-    tweet=pattern.sub(r"\1",tweet)
 
-    return removeStopWords(tweet)
+    #load the stopwords (taken from nltk)
+    def load_stopwords(self):
+        self.stopwords=set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
+'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers',
+'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
+'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
+'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
+'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
+'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
+'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
+'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
+'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
+'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
+'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now'])
 
-# end
+
+    def lex_process(self, text):
+        words=[]
+        for line in text.splitlines():
+            for w in line.split():
+                words.append(w)
+        return self.stemming(words)
 
 
 
-#remove the stopwords and set it to a dict
-def removeStopWords(tweet):
-    stop_words=loadStopwords()
-    tweet=tweet.split()
-    output=[word for word in tweet if word not in stop_words and len(word)>2]
-    return output
+    def remove_stopwords(self, words):
+        return [w for w in words if w not in self.stopwords]
+
+
+
+    def stemming(self, words):
+        ps=LancasterStemmer()
+        print [ps.stem(w) for w in words]
+        return self.remove_stopwords([ps.stem(w) for w in words])
+
+
+    def enc_json(self, terms):
+        output=JSONEncoder.encode({'terms':terms})
+        print output
+        return output
 
 
 
 
 
 
-# tweets=load()
-# wf=open('proc_tweets.txt', 'w')
-# for line in tweets:
-#     sent, tweet=line.keys()[0], line.values()[0]
-#     f_t=''
-#     for w in tweet:
-#         f_t+=' '+w
-#     wf.write(str(sent)+','+f_t+'\n')
-# wf.close()
-# a=1+2
+
+
+
+
+    # def load_docs(self,docs_path="data\\docs"):
+    #     docs=[doc for doc in listdir(docs_path)]
+    #     self.docs_dics=[]
+    #     for doc in docs:
+    #         self.docs_dics.append(self.load(docs_path,doc))
+    #     file=open('data\\logs.txt', 'w')
+    #     file.write(str(self.docs_dics))
+    #     file.close()
+    #
+    #
+    # def load(self,docs_path,doc):
+    #     word_counter={}
+    #     tl=open(docs_path+'\\'+doc, 'r')
+    #     line=tl.readline()
+    #     while line:
+    #         proc_line=self.process_text(line)
+    #         self.word_counter(proc_line, word_counter)
+    #         line=tl.readline()
+    #     tl.close()
+    #     return word_counter
+    #
+    # def word_counter(self, line, words_counter_dic):
+    #     for word in line:
+    #         if word in words_counter_dic.keys():
+    #             words_counter_dic[word]+=1
+    #         else:
+    #             words_counter_dic[word]=1
+    #
+    # # start process_tweet
+    # # **taken from internet
+    # def process_text(self,text):
+    #     # Convert to lower case
+    #     text = text.lower()
+    #     # Remove additional white spaces
+    #     text = re.sub('[\s]+', ' ', text)
+    #     # trim
+    #     text = text.strip('\'"')
+    #
+    #     #delete not letter chars
+    #     text=re.sub('[=|+|,|.|?|!|*|;|:|(|)|&|-|\|[|\]]','', text)
+    #
+    #     #delete consecutive chars with the same value
+    #     pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    #     text=pattern.sub(r"\1",text)
+    #
+    #     return self.remove_stop_words(text)
+    #
+    # # end
+    #
+    #
+    #
+    # #remove the stopwords and set it to a dict
+    # def remove_stop_words(self, text):
+    #     text=text.split()
+    #     output=[word for word in text if word not in STOP_WORDS and len(word)>2]
+    #     return output
+
+text="What doing this morning \n was something outing of going to \n ripping".encode()
+print text
+proc=Text_Processor().process('{"action":"process", "data":"What doing this morning was something outing of going to  ripping"}')
+print proc
+
